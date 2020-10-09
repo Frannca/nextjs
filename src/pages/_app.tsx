d56b06f3ab6dd1e1e4ea255cb64c33e32b2456b1
Renato França <renatofrancarpmf@gmail.com>
2020-10-09 00:36:25 -0300

    Revert "Development (#2)"
    This reverts commit 19ce6bff09e24da30a0ea0f639214aab3fdff432.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,8 @@
-import React from 'react'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import '../../styles/globals.css'
 
-import GlobalStyles from 'styles/global'
-import { ThemeProvider } from '@material-ui/core/styles'
-import theme from 'styles/theme'
-import { CssBaseline } from '@material-ui/core'
-
-function App({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles)
-    }
-  }, [])
-
-  return (
-    <>
-      <Head>
-        <title>Brava500</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="description" content="Site da Brava 500." />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default App
+export default MyApp
