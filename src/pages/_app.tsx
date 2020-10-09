19ce6bff09e24da30a0ea0f639214aab3fdff432
Renato França <renato@frannca.com>
2020-10-09 00:32:40 -0300

    Development (#2)
    * refactor: remove pages/api
    
    * fix: put back pages folder
    
    * chore: configure TypeScript
    
    * chore: enable TypeScript strict mode
    
    * refactor: create src folder and move pages folder into it
    
    * refactor: rename js files to tsx
    
    * chore: configure eslint
    
    * chore: configure react hooks eslint plugin
    
    * chore: add eslint rules and configure detect react version
    
    * chore: configure eslint for prettier
    
    * chore: install prettier
    
    * chore: create VS Code configuration file
    
    * chore: configure husky to work with lint staged
    
    * chore: configure jest and babel
    
    * feat: storybook configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,38 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
-import '../../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from 'styles/theme'
+import { CssBaseline } from '@material-ui/core'
+
+function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Brava500</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="description" content="Site da Brava 500." />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default MyApp
+export default App
