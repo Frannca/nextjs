a7b6a41a9f5ece34e244938837a8a10cfc22ff3b
Renato França <renato@frannca.com>
2020-10-12 17:15:14 -0300

    refactor: add login form and welcome text

--- src/components/Login/test.tsx
+++ src/components/Login/test.tsx
@@ -9,4 +9,24 @@ describe('<Login />', () => {
   it('should render a <Login /> component', () => {
     expect(screen.getByTestId('login')).toBeVisible()
   })
+
+  it('should have a welcome title', () => {
+    expect(screen.getByText('Welcome to the Frannca Dashboard!')).toBeVisible()
+  })
+
+  it('should have a login title', () => {
+    expect(screen.getByText('Login to your account')).toBeVisible()
+  })
+
+  it('should have an email input', () => {
+    expect(screen.getByLabelText('Email *')).toBeVisible()
+  })
+
+  it('should have a password input', () => {
+    expect(screen.getByLabelText('Password *')).toBeVisible()
+  })
+
+  it('should have a login button', () => {
+    expect(screen.getByRole('button', { name: /login/i })).toBeVisible()
+  })
 })
